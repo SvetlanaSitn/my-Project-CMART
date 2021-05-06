bc8e9c13a8b44e4e1ea534127e150979616f71c7
SvetlanaSitn <46673852+SvetlanaSitn@users.noreply.github.com>
2021-05-06 17:01:11 +0300

    Add files via upload

---
+++ Открытый урок.py
@@ -0,0 +1,102 @@
+from tkinter import *
+
+def button_press():
+    print("Правильно")
+    
+tk = Tk()
+a = Button(tk, text= "4", background="blue",foreground="lime",padx="1000", pady="50", font="50", command=button_press)
+a.pack()
+
+def button_press():
+    print("Не правильно")
+
+
+tk.geometry("300x250")
+ 
+a = Button(tk, text="6",background="blue",foreground="lime",padx="1000", pady="50", font="50", command=button_press)
+a.pack()
+##########################################################################################################################################
+##########################################################################################################################################
+def button_press():
+    print("Правильно")
+
+tk = Tk()   
+
+b = Button(tk, text= "7", background="blue",foreground="lime",padx="1000", pady="50", font="50", command=button_press)
+b.pack()
+
+def button_press():
+    print("Не правильно")
+
+
+tk.geometry("300x250")
+ 
+b = Button(tk, text="4",background="blue",foreground="lime",padx="1000", pady="50", font="50", command=button_press)
+b.pack()
+##########################################################################################################################################
+##########################################################################################################################################
+def button_press():
+    print("Правильно")
+
+tk = Tk()   
+
+b = Button(tk, text= "9", background="blue",foreground="lime",padx="1000", pady="50", font="50", command=button_press)
+b.pack()
+
+def button_press():
+    print("Не правильно")
+
+
+tk.geometry("300x250")
+ 
+b = Button(tk, text="3",background="blue",foreground="lime",padx="1000", pady="50", font="50", command=button_press)
+b.pack()
+##########################################################################################################################################
+##########################################################################################################################################
+def button_press():
+    print("Правильно")
+
+tk = Tk()   
+
+b = Button(tk, text= "9", background="blue",foreground="lime",padx="1000", pady="50", font="50", command=button_press)
+b.pack()
+
+def button_press():
+    print("Не правильно")
+
+
+tk.geometry("300x250")
+ 
+b = Button(tk, text="3",background="blue",foreground="lime",padx="1000", pady="50", font="50", command=button_press)
+b.pack()
+##########################################################################################################################################
+##########################################################################################################################################
+def button_press():
+    print("Правильно")
+
+tk = Tk()   
+
+b = Button(tk, text= "9", background="blue",foreground="lime",padx="1000", pady="50", font="50", command=button_press)
+b.pack()
+
+def button_press():
+    print("Не правильно")
+
+
+tk.geometry("300x250")
+ 
+b = Button(tk, text="3",background="blue",foreground="lime",padx="1000", pady="50", font="50", command=button_press)
+b.pack()
+
+
+
+
+
+
+
+
+
+
+
+
+
